4141ba1ebef3fe4a62e33d6c60495422d38f5871
ezratameno <ezratameno@gmail.com>
2022-06-13 17:15:02 +0300

    fix addind to cost collection

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -6,7 +6,6 @@ const Costs = mongoose.model('costs');
 module.exports = function generalRoutes(app) {
     // will show the cost items of the user
     app.get("/home",function(req,res){
-        console.log(req.session)
         res.render("home")
     });
 
@@ -16,6 +15,7 @@ module.exports = function generalRoutes(app) {
     })
 
     app.post("/create", async (req,res) => {
+        // cost item that we got from the user
         let cost = {
             sum: req.body.sum,
             description: req.body.description,
@@ -25,7 +25,7 @@ module.exports = function generalRoutes(app) {
         }
 
         // find the user by id in costs
-        // if not exist than create a new cost entry
+        // if not exist than create a new cost entry for the user
         let userCostEntry
         try{
             userCostEntry = await Costs.findOne({"id": req.session.user.user.id})
@@ -33,8 +33,10 @@ module.exports = function generalRoutes(app) {
             res.send(err)
         }
 
+        let entryForUserExist = true;
         // if user not exist
         if (!userCostEntry) {
+            entryForUserExist = false;
             // create cost entry
             userCostEntry = {
                 id: String,
@@ -43,39 +45,48 @@ module.exports = function generalRoutes(app) {
             userCostEntry.id = req.session.user.user.id
             userCostEntry.costs = new Map()
 
-        }
+        } 
 
-
+        // month and year
         let date = cost.date.split('-')[1] + "-" + cost.date.split('-')[0]
-        // if we don't have any entries for this month and year
-        if (typeof userCostEntry.costs[date] == 'undefined') {
+
+        // if we don't have entry in the cost collection for this user,
+        // than we need to handle the access to the data diffrently
+        if (!entryForUserExist) {
             userCostEntry.costs[date] = {
                 items: new Array(),
                 total: Number 
             }
-            userCostEntry.costs[date].total = 0 
-        }
-        userCostEntry.costs[date].items.push(cost)
+            userCostEntry.costs[date].total = 0
+            userCostEntry.costs[date].items.push(cost)
+            // add to the map
+            userCostEntry.costs.set(date,{
+                items: userCostEntry.costs[date].items,
+                total: userCostEntry.costs[date].total += Number(cost.sum)
+            });
+        } else {
+            // if the user already have an entry on the costs collection but don't have in this month and year
+            if(typeof userCostEntry.costs.get(date) == 'undefined') {
+                userCostEntry.costs.set(date,{
+                    items: new Array(),
+                    total: 0,
+                });
+            }
+            // update the items and the total amout that we spent in this mont and year
+            userCostEntry.costs.get(date).items.push(cost) 
+            userCostEntry.costs.get(date).total += Number(cost.sum)
 
-        // add to the map
-        userCostEntry.costs.set(date,{
-            items: userCostEntry.costs[date].items,
-            total: userCostEntry.costs[date].total += Number(cost.sum)
-        })
-        
-        
+        }
+    
+        // save into the costs collection
         try {
             const newCostEntry = new Costs(userCostEntry)
-
-            console.log(userCostEntry)
-            console.log(newCostEntry)
-
             await newCostEntry.save();
         } catch(err) {
             // TODO: create custome errors or error page
             res.send(err)
         }
-      
+        // TODO: create a page that will show that we successfuly created a cost item
         res.send(userCostEntry)
 
         // need to extract the 
